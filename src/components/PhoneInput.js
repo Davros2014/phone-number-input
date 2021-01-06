14a8157f0a94e7739cd14ceccde206238b961df8
David Parsons <info@dpdesigns.eu>
2021-01-06 23:03:41 +0100

    css: basic styling added

--- src/components/PhoneInput.js
+++ src/components/PhoneInput.js
@@ -3,9 +3,9 @@ import React from "react";
 export const PhoneInput = () => {
   return (
     <div className="inputContainer">
-      <h3 className="inputContainer__Header">
-        Please add your phone number here and we'll get back to you promptly
-      </h3>
+        <h3 className="inputContainer__Header">
+            Please submit your phone number here and we'll get back to you promptly
+        </h3>
 
       <form action="" className="inputContainer__Form">
         <div className="inputContainer__phoneDetails">
