420eaa03186336be4def7e62c1270038282e5250
David Parsons <info@dpdesigns.eu>
2021-01-06 23:46:47 +0100

    jsx: added some validation & regex to clean input

--- src/components/PhoneInput.js
+++ src/components/PhoneInput.js
@@ -1,30 +1,61 @@
 import React, { useState } from "react";
 
 export const PhoneInput = () => {
-  const [value, setValue] = useState("");
+  const [number, setNumber] = useState("");
+  const [error, setError] = useState("");
+  const [plusOneNumber, setPlusOneNumber] = useState("");
 
-  const handleChange = e => {
-    console.log("e", e);
-    setValue(e);
+  const handleChange = number => {
+    console.log("e", number);
+    let digitsOnlyNumber = ("" + number).replace(/\D/g, "");
+    let usNumber = "+1" + digitsOnlyNumber;
+
+    validate(usNumber);
+    setNumber(number);
   };
+  const validate = usNumber => {
+    usNumber.length > 10
+      ? setError(
+          "Sorry, your number is invalid, please enter a valid US number"
+        )
+      : setError("");
+  };
+  const reset = () => {
+    setNumber("");
+    setPlusOneNumber("");
+  };
+
   return (
     <div className="inputContainer">
-        <h3 className="inputContainer__Header">
-            Please submit your phone number here and we'll get back to you promptly
-        </h3>
+      <h3 className="inputContainer__Header">
+        Please submit your phone number here and we'll get back to you promptly
+      </h3>
 
-        <form action="" className="inputContainer__Form">
-            <div className="inputContainer__phoneDetails">
-                <input
-                    type="string"
-                    className="inputContainer__Input"
-                    placeholder="Please add your phone number here - (xxx) xxx-xxxx"
-                    value={value}
-                    onChange={e => handleChange(e.target.value)}
-                />
-            </div>
-            <p className="inputContainer__OutputField">{value}</p>
-            <button type="submit" className="inputContainer__Btn">
+      <form
+        className="inputContainer__Form"
+        onSubmit={e => {
+          e.preventDefault();
+          reset();
+        }}
+      >
+        <div className="inputContainer__phoneDetails">
+          <input
+            type="string"
+            className="inputContainer__Input"
+            placeholder="Please add your phone number here - (xxx) xxx-xxxx"
+            value={number}
+            onChange={e => handleChange(e.target.value)}
+          />
+        </div>
+        <p className="inputContainer__OutputField">
+          <span>Value: {number}</span>
+        </p>
+        {error && (
+          <>
+            <p className="errorMessage">{error}</p>
+          </>
+        )}
+        <button type="submit" className="inputContainer__Btn">
           Reset
         </button>
       </form>
